5b539c297fdff77850671c3d79199f21b26c3c9b
Tino Rusch <tino.rusch@webvariants.de>
2016-05-31 09:01:49 +0200

    initial commit

---
+++ src/Greeter/index.js
@@ -0,0 +1,8 @@
+export default class Greeter {
+  constructor(message){
+    this.message = message;
+  }
+  greet(){
+    console.log(this.message);
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Greeter from './Greeter';
+
+function main(){
+  let g = new Greeter('Hello World!');
+  g.greet();
+}
+
+main();
